6bd045b59a6aa20f4ee811d8aaeb22874e7db9f0
squish3993 <jdlewis@wesleyan.edu>
2017-11-08 22:40:00 -0500

    changed views file and migrated tables

---
+++ database/migrations/2017_11_09_031110_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('eventName');
+            $table->string('name');
+            $table->string('Department');
+            $table->dateTime('dateAndTime');
+            $table->string('location');
+            $table->text('specs');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
